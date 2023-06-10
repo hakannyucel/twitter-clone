4b8b3850bd041a36c067f5fe7a8dcde4069dcf3b
hakannyucel <hakanyucelsw@gmail.com>
2023-06-11 01:13:14 +0300

    Update Links

--- src/pages/feed/components/FeedItem.tsx
+++ src/pages/feed/components/FeedItem.tsx
@@ -16,11 +16,11 @@ const FeedItem: React.FC<FeedItemProp> = ({fullName, username, content, likeCoun
     return (
         <div className='flex p-4 pb-2 border-b-2 border-gray-800'>
             <div>
-                <img src="/assets/profile.jpg" alt="" width={40} height={40} className='rounded-full' />
+                <img src="/twitter-clone/assets/profile.jpg" alt="" width={40} height={40} className='rounded-full' />
             </div>
             <div className='flex flex-col space-y-1 px-2 w-full '>
                 <div className='flex justify-between items-center'>
-                    <a href="/profile">
+                    <a href="/twitter-clone/profile">
                         <span className='font-bold text-white hover:underline transition'>{fullName}</span>  <span className='text-gray-500 text-sm'>{username}</span>
                     </a>
                     <div className='cursor-pointer hover:bg-gray-800 transition rounded-full'><AiOutlineEllipsis size={25} color='#ddd' /></div>
