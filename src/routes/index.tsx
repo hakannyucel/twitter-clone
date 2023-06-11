0383a62b1e0c53c3f57e5bbc1abb7d225b08c1a2
hakannyucel <hakanyucelsw@gmail.com>
2023-06-11 10:38:08 +0300

    Test Route Path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,15 @@
 import React, { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import PageNotFound from "../pages/notFound";
 
 const Feed = lazy(() => import("../pages/feed"));
 const Explore = lazy(() => import("../pages/explore"));
+const PageNotFound = lazy(() => import("../pages/notFound"));
 
 const AppRoutes: React.FC = () => {
     return (
         <Routes>
-            <Route path="/" element={<Feed />} />
             <Route path="/explore" element={<Explore />} />
+            <Route path="/" element={<Feed />} />
             <Route path="/*" element={<PageNotFound />} />
         </Routes>
     )
