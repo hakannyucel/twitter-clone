1b12a4db154524199a266cfbd7a2c7a2cf4d4205
hakannyucel <hakanyucelsw@gmail.com>
2023-06-17 01:27:37 +0300

    Update Sidebar Position (Fixed)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,24 +6,26 @@ import SidebarProfile from './SidebarProfile';
 const Sidebar: React.FC = () => {
 
     return (
-        <div className='w-4/12 hidden flex-col px-4 sm:flex'>
-            <div>
-                <a href="/" className='inline-block hover:bg-gray-800 transition rounded-full p-2 m-2'>
-                    <AiOutlineTwitter size={30} color='#ddd' />
-                </a>
-            </div>
-            <div className='flex flex-col'>
-                <SidebarMenuButton icon={<AiOutlineHome size={30} color='#ddd' />} link='/' title={"Home"} hasNotification={true} />
-                <SidebarMenuButton icon={<AiOutlineSearch size={30} color='#ddd' />} link='/explore' title={"Explore"} />
-                <SidebarMenuButton icon={<AiOutlineBell size={30} color='#ddd' />} link='#' title={"Notification"} />
-                <SidebarMenuButton icon={<AiOutlineUser size={30} color='#ddd' />} link='#' title={"Profile"} />
-                <SidebarMenuButton icon={<AiOutlineEllipsis size={30} color='#ddd' />} link='#' title={"More"} />
-            </div>
-            <div className='mt-3'>
-                <button className='py-3 px-10 bg-blue-400 rounded-3xl text-white font-bold w-full tracking-wider hover:brightness-75 transition'>Tweet</button>
-            </div>
+        <div className='hidden w-4/12 sm:block'>
+            <div className='flex flex-col fixed px-4'>
+                <div>
+                    <a href="/" className='inline-block hover:bg-gray-800 transition rounded-full p-2 m-2'>
+                        <AiOutlineTwitter size={30} color='#ddd' />
+                    </a>
+                </div>
+                <div className='flex flex-col'>
+                    <SidebarMenuButton icon={<AiOutlineHome size={30} color='#ddd' />} link='/' title={"Home"} hasNotification={true} />
+                    <SidebarMenuButton icon={<AiOutlineSearch size={30} color='#ddd' />} link='/explore' title={"Explore"} />
+                    <SidebarMenuButton icon={<AiOutlineBell size={30} color='#ddd' />} link='#' title={"Notification"} />
+                    <SidebarMenuButton icon={<AiOutlineUser size={30} color='#ddd' />} link='#' title={"Profile"} />
+                    <SidebarMenuButton icon={<AiOutlineEllipsis size={30} color='#ddd' />} link='#' title={"More"} />
+                </div>
+                <div className='mt-3'>
+                    <button className='py-3 px-10 bg-blue-400 rounded-3xl text-white font-bold w-full tracking-wider hover:brightness-75 transition'>Tweet</button>
+                </div>
 
-            <SidebarProfile />
+                <SidebarProfile />
+            </div>
         </div>
     )
 }
--- src/components/Trends.tsx
+++ src/components/Trends.tsx
@@ -7,17 +7,15 @@ interface TrendsProp {
 
 const Trends: React.FC<TrendsProp> = ({backgroundColor}) => {
     return (
-        <div>
-            <div className={`rounded-xl my-3 ${backgroundColor}`}>
-                <p className='font-semibold text-xl p-3'>Trends for you</p>
-                <div className='flex flex-col'>
-                    <TrendItem trend='Trending in Turkey' tag='#altcoin' tweets='70.2k tweets' />
-                    <TrendItem trend='Trending in Turkey' tag='#istanbul23' tweets='60.2k tweets' />
-                    <TrendItem trend='Trending in Turkey' tag='Sabiha Gökçen' tweets='20.2k tweets' />
-                    <TrendItem trend='Trending in Turkey' tag='Talisca' tweets='15k tweets' />
-                </div>
-                <a href="/#" className='p-3 w-full block text-blue-500 text-sm hover:bg-gray-800 transition rounded-lg rounded-t-none'>Show more</a>
+        <div className={`rounded-xl my-3 ${backgroundColor}`}>
+            <p className='font-semibold text-xl p-3'>Trends for you</p>
+            <div className='flex flex-col'>
+                <TrendItem trend='Trending in Turkey' tag='#altcoin' tweets='70.2k tweets' />
+                <TrendItem trend='Trending in Turkey' tag='#istanbul23' tweets='60.2k tweets' />
+                <TrendItem trend='Trending in Turkey' tag='Sabiha Gökçen' tweets='20.2k tweets' />
+                <TrendItem trend='Trending in Turkey' tag='Talisca' tweets='15k tweets' />
             </div>
+            <a href="/#" className='p-3 w-full block text-blue-500 text-sm hover:bg-gray-800 transition rounded-lg rounded-t-none'>Show more</a>
         </div>
     )
 }
