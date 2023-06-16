f86d79cdd4f062434710290075cd94963aab6cc8
hakannyucel <hakanyucelsw@gmail.com>
2023-06-17 00:37:05 +0300

    Action Button Text Color Active Settings

--- src/components/SingleActionButton.tsx
+++ src/components/SingleActionButton.tsx
@@ -21,7 +21,7 @@ const SingleActionButton: React.FC<ISingleActionButton> = ({ onClick, icon, text
     }
 
     return(
-        <motion.div className='flex space-x-2 px-2 md:px-4 py-2 cursor-pointer sm:hover:bg-gray-900 sm:active:bg-none active:bg-gray-900 hover:text-blue-500 transition rounded-full' 
+        <motion.div className='flex space-x-2 px-2 md:px-4 py-2 cursor-pointer sm:hover:bg-gray-900 sm:active:bg-none active:bg-gray-900 active:text-blue-500 sm:hover:text-blue-500 transition rounded-full' 
             onClick={() => {
                 setIsClicked(!isClicked);
                 onClick();
